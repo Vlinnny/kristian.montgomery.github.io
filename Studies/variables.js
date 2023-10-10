e4356d900832f8d10120454c1d41970f3bb2b783
Kristian Montgomery <montgomery.kristian1@gmail.com>
2023-10-10 17:25:40 +0000

    studies

--- Studies/variables.js
+++ Studies/variables.js
@@ -1,8 +1,18 @@
 /**
  * VARIABLES:
  * 
- * 0:
- * 1: Delcaration and assignment
- * 2: Var, let, and const
- * 3: Hoisting (describe hoisting)
+ * 0: In JavaScript variables are used as containers for storing values, var is one of three keywords that can be used to
+ * declare a variable. All JavaScript variables must be identified with unique names, called identifiers, it's good practice
+ * to declare these identifiers in camel-case; which is a naming convinction, in which we combine multiple words and make one
+ * string by capitalizing the first character of each string except for the first. 
+ * 
+ * 1: After we declare a variable with one of our three keywords and create the identifier, we need to assign a value to
+ * the variable. Variables can be assigned to many types of data such as strings, numbers, arrays, booleans etc... 
+ * 
+ * 2: There are three keywords to declare a variable in JavaScript, var, let, and const. Out of the three keywords it is
+ * more preffered to use let or const when declaring a variable, this is because var can be re-declared to a different value,
+ * while let or const can not.
+ * 
+ * 3: Hoisting is a JavaScript mechanism where variable and function declarations are moved to the top of their respective
+ * scopes before execute.
  */
